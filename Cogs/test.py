aea6412acd95a41e0bc97cd657b04d34fd1bbc3a
Chris Scaramella <CScarame@nd.edu>
2019-05-05 13:22:26 -0700

    Added Basic Cog and Logging

--- Cogs/test.py
+++ Cogs/test.py
@@ -1,12 +1,17 @@
 import discord
 from discord.ext import commands
 
+test_help = """ This is a test command that simply repeats back what you say."""
+
 
 class TestCog(commands.Cog):
+    """ This is a test cog with only one test command"""
+    
     def __init__(self, bot):
         self.bot = bot
+        
 
-    @commands.command()
+    @commands.command(help=test_help)
     async def test(self, ctx, *, test : str):
         await ctx.send(test)
 
--- new_main.py
+++ new_main.py
@@ -1,12 +1,28 @@
 import discord
 from   discord.ext import commands
 
+import logging
+import os
 
 bot = commands.Bot(command_prefix='!')
 
 @bot.event
 async def on_ready():
+    logging.debug("Discord connection made.")
+    logging.info("Logging in as:{0} : {1}".format(bot.user.name,bot.user.id))
     print("Logged in as\n{0}\n{1}".format(bot.user.name,bot.user.id))
-    bot.load_extension("Cogs.test")
-bot.run('NDM4Mjg4NjYxMzk4NjgzNjUw.DcCb5A.gyiEoXkZyEnnByhjOXshRriRHXY')
+    bot.load_extension("Cogs.Basic")
+
+@bot.event
+async def on_command(ctx):
+    logging.info("Received |{0.command.name}| command from |{0.message.author.name}|".format(ctx))
+    print("Command Received")
+
+if __name__ == "__main__":
+    ## TODO: Set logging level to DEBUG if command line args include --debug or -d
+
+    logging.basicConfig(filename="runtime.log",level=logging.INFO)
+    logging.debug("Starting program")
+    bot.run('NDM4Mjg4NjYxMzk4NjgzNjUw.DcCb5A.gyiEoXkZyEnnByhjOXshRriRHXY')
+
 
